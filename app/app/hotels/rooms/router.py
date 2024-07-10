7cf5f0e1f532f7e4f16e7991247fc1ef54b8cb01
Dar1usss <118019309+danissimoae@users.noreply.github.com>
2024-07-11 04:38:13 +0500

    Delete app/app directory

--- app/app/hotels/rooms/router.py
+++
@@ -1,6 +0,0 @@
-from app.hotels.router import router
-
-
-@router.get('/{hotel_id}/rooms')
-def get_rooms():
-    pass
--- app/app/users/data_access_object.py
+++
@@ -1,5 +0,0 @@
-from app.data_access_object.base import Base_Data_Access_Object
-from app.users.models import Users
-
-class Users_Data_Access_Object(Base_Data_Access_Object):
-    model = Users
